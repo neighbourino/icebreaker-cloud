753ffa6810640aaf7b84b9720155fa970b9acc8c
Janus Helkjær <jh@mindmill.dk>
2024-08-13 20:58:49 +0200

    api auth routes + controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\PostController;
 
 
@@ -13,9 +14,19 @@ Route::group(['prefix' => 'v1'], function () {
     // Route::post('/posts', [PostController::class, 'store'])->name('api.posts.store');
     // Route::put('/posts/{customer}', [PostController::class, 'update'])->name('api.posts.update');
     // Route::delete('/posts/{customer}', [PostController::class, 'destroy'])->name('api.posts.destroy');
+
+
+    // login route
+    Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('api.forgot-password');
+
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->middleware('auth:sanctum');
+
+        Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
+    });
 });
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
